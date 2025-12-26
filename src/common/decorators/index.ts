d4389f97ec045710146b457869e0371e4305c333
kregaieg <43063549+kregaieg@users.noreply.github.com>
2025-12-26 15:26:28 +0100

    remove decorator only for response

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -2,5 +2,5 @@
 // SOFT-M - Common Decorators Barrel Export
 // ============================================
 
-
+export * from './api-common-responses.decorator';
 export * from './is-cuid.decorator';
